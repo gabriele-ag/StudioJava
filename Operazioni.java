c7c6e3b73f414703bfea4cb37a48e9d4bdb72bff
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2026-02-20 10:13:18 +0100

    feat: studyng Operations on Java

---
+++ Operazioni.java
@@ -0,0 +1,33 @@
+public class Operazioni {
+    public static void main(String[] args) {
+        // int x = 10;
+        // int y = 3;
+        // int z = x % y;
+
+        // int g = 10;
+        // g %= 3;
+
+        // int u = 11;
+        // u++;
+        // u++;
+        // u++;
+        // u++;
+
+        // int risultato = ((x + y) * u - (3 - 2 + g));
+        // risultato++;
+
+        System.out.println(Math.abs(-55));
+        System.out.println(Math.min(55,21));
+        System.out.println(Math.max(55,21));
+        System.out.println(Math.pow(3,2));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.PI);
+
+        // Consulta sempre la documentazione per vedere gli altri Math!
+
+        // System.out.println(z);
+        // System.out.println(g);
+        // System.out.println(u);
+        // System.out.println(risultato);
+    }
+}
