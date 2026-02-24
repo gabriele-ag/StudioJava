d56c9d3d210d04603878babbe74d13bf0a48abf2
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2026-02-24 11:23:14 +0100

    feat: updated mainpizza

--- MainPizza.java
+++ MainPizza.java
@@ -1,9 +1,11 @@
 public class MainPizza {
     
     public static void main(String[] args) {
-        Pizza pizza1 = new Pizza("integrale", "pomodoro", "mozzarella", "patatine");
+        Pizza pizza1 = new Pizza("integrale", "pomodoro");
         Pizza pizza2 = new Pizza("5 cereali");
-        Pizza pizza3 = new Pizza("normale", "pomodoro", "mozzarella");
+        Pizza pizza3 = new Pizza("integrale", "pomodoro", "caciovallo", "prosciutto", "funghi") ;
         System.out.println(pizza1);
+        System.out.println(pizza2);
+        System.out.println(pizza3);
     }
 }
