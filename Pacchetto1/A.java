c588fdb0e5a06c5483522e56a028a891dc650579
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2026-02-26 16:47:43 +0100

    feat: Getters & Setters done

--- Pacchetto1/A.java
+++ Pacchetto1/A.java
@@ -3,6 +3,22 @@ package Pacchetto1;
 import Pacchetto2.*;
 
 public class A {
-    protected String nome = "Giovanni";
-    private String nome2 = "Alice";
+    protected String nome;
+    private String cognome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
 }
--- Pacchetto1/Main.java
+++ Pacchetto1/Main.java
@@ -6,6 +6,7 @@ public class Main {
    public static void main(String[] args) {
        A prova = new A();
 
-       System.out.println(prova.nome);
+       prova.setCognome("Verdi");
+       System.out.println(prova.getCognome());
    }
 }
